ae02bb01837d4d5ee649303b056e521bf304834c
lerawilson <lerawilson@localhost>
2023-03-30 14:15:46 -0500

    finished

--- src/studio7/die.java
+++ src/studio7/die.java
@@ -1,8 +1,19 @@
 package studio7;
 
 public class die {
+	private int n;
+	public die(int theN) {
+		theN=n;
+		
+	}
+	public int getDie() {
+		int throw = (int)(Math.random()*n);
+		return throw;
+		
+	}
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 
 	}
